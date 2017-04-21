15e0db0b60af065e595b8d6636c21985b7cf434f
MC 子龙 <x-jesse@foxmail.com>
2017-04-21 22:07:30 +0800

    ..

--- Eglish help learn/src/tool/Panel1.java
+++ Eglish help learn/src/tool/Panel1.java
@@ -7,11 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
-
 import english_reader.window;
 import englishchinese.select_chinese_window;
 import mian.Mianwindow;
